7d8fbd0dc00acae4568c56d48ea2a72348335e00
Flavio Miceli <flavio.armando.miceli@gmail.com>
2020-11-18 11:44:33 +0100

    Started implementation of ngram language model

--- n_gram.py
+++ n_gram.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-class CORPUS:
+class Corpus:
 
 	def __init__(self, path, epi):
 		self._count_table = defaultdict(int)
@@ -10,9 +10,8 @@ class CORPUS:
 		self.set_corpus(path, epi)
 		self.update_count_table()
 
-
-	# TODO: use a generator to yield lines
-	def set_corpus(self, path, epi_code, use_dict=True):
+	def set_corpus(self,
+			path, epi_code, dict_path='g2p_dictionary/dutch_dic_to_phonetic.1.json'):
 		def clean_line(line):
 			import string
 
@@ -28,15 +27,13 @@ class CORPUS:
 
 		import json
 		from epitran import Epitran
-
 		with open(path, 'r') as text, \
-			open('g2p_dictionary/dutch_dic_to_phonetic.1.json', 'r') as g2p_file:
-			lines = text.readlines()
+			open(dict_path, 'r') as g2p_file:
 			g2p_dict = json.load(g2p_file)
 			g2p_dict = {k.lower():v for k,v in g2p_dict.items()}
 			epi = Epitran(epi_code)
 
-			for line in lines:
+			for line in text:
 				line = clean_line(line)
 				# transliterate g2p using dict with epitran as fallback
 				if (use_dict):
@@ -51,18 +48,23 @@ class CORPUS:
 					self._corpus.append(line_phonetic)
 
 	def update_count_table(self):
-		# Update count table
+		# Go through corpus line by line. Add occurences of each word to table.
 		self._count_table = defaultdict(int)
 		for line in self._corpus:
 			for word in line.split():
 				self._count_table[word] += 1
 
-		# Update vocab
 		self._vocab = set(self._count_table.keys())
 
-	def rm_lines_with_rare_words(self, min_count=1):
+	def prune_corpus(self, min_count=1, min_length=1):
+		if (min_count > 1):
+			self.rm_lines_with_rare_words(min_count)
+		if (min_length > 1):
+			self.rm_short_lines(min_length)
 
-		# store lines that do not contain rare words
+	def rm_lines_with_rare_words(self, min_count=1):
+		# Store each line in corpus where every word occurs at least min_count times
+		# in the original corpus.
 		self._corpus = [line for line in self._corpus if \
 			all([self._count_table[word] >= min_count for word in line.split()])]
 		self.update_count_table()
@@ -77,24 +79,67 @@ class CORPUS:
 			print (line)
 
 	def print_vocab(self):
-		print(self._vocab)
+		print(sorted(self._vocab))
+
+class NGram_LM:
+
+	def __init__(self, order):
+		self._order = order
+		self._count_table = dict()
+		self._prob_table = dict()
+
+	def preprocess_sentence(self, text):
+		for line in text:
+			yield [c for c in line]
+
+	def preprocess_history(self, history):
+		# Used to select the end of a longer sentence, or pad the start of a
+		# sentence with <s> tokens
+		if (len(history) == self._order):
+            return (tuple(history))
+		elif (len(history) > self._order):
+            return (tuple(history[len(history) - self._order:length]))
+		else:
+            missing = self._order - len(history)
+            return (tuple(['<s>'] * missing) + tuple(history))
+
+	def count_ngrams(self, text):
+		for line in text:
+			for i in range(len(line)):
+				phoneme = line[i]
+				ngram = self.preprocess_history(line[:i])
+				if ngram in self._count_table:
+					if phoneme in self._count_table[ngram]:
+                        self._count_table[ngram][phoneme] += 1
+                    else:
+                        self._count_table[ngram][phoneme] = 1
+				else:
+                    self._count_table[ngram] = {phoneme: 1}
+
+	def set_prob_table(self):
+		self._prob_table = self._count_table
+
+		for ngram, countdict in self._count_table.items():
+			ngram_count = sum([count for count in countdict.values()])
+			for word, count in countdict.items():
+				self._prob_table[ngram][word] = count / ngram_count
 
 def main():
 	# parsing of input flags
 	import argparse
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-v", "--verbose", nargs='?', const=1, type=int,
-					help="output additional information")
-	parser.add_argument("-c", "--count", type=int,
-					help="Prune sentences with words that occur less than COUNT times in corpus.")
-	parser.add_argument("-l", "--length", type=int,
-					help="Prune sentences that have fewer than LENGTH words.")
+		help="output additional information")
+	parser.add_argument("-c", "--count", type=int, default=1,
+		help="Prune sentences with words that occur less than COUNT times in corpus.")
+	parser.add_argument("-l", "--length", type=int, default=1,
+		help="Prune sentences that have fewer than LENGTH words.")
 	parser.add_argument("file", type=str,
-                    help="input file used as training data")
+        help="input file used as training data")
 	args = parser.parse_args()
 
 	# Init language model
-	corp = CORPUS(args.file, "nld-Latn")
+	corp = Corpus(args.file, "nld-Latn")
 
 	# Output corpus info if verbose
 	if (args.verbose):
@@ -103,10 +148,7 @@ def main():
 				.format(len(corp._corpus), len(corp._vocab)))
 
 	# Prune sentences
-	if (args.count):
-		corp.rm_lines_with_rare_words(args.count)
-	if (args.length):
-		corp.rm_short_lines(args.length)
+	corp.prune_corpus(min_count=args.count, min_length=args.length)
 
 	# Output corpus info if verbose
 	if (args.verbose):
--- tests.py
+++ tests.py
@@ -16,15 +16,15 @@ def compare_epi_to_dict():
 	dict_file = open('g2p_dictionary/dutch_dic_to_phonetic.1.json', 'r')
 	g2p_dict = json.load(dict_file)
 	epi = Epitran('nld-Latn')
-	# n = 0
+	n = 0
 	for key in g2p_dict:
 		dic_word = g2p_dict[key]
 		epi_word = epi.transliterate(key)
 		if (dic_word != epi_word):
-			# n += 1
-			fstring = "Key: %s\nDic: %s\nEpi: %s\n" % (key, dic_word, epi_word)
-			print (fstring)
-	# print (n)
+			n += 1
+			# fstring = "Key: %s\nDic: %s\nEpi: %s\n" % (key, dic_word, epi_word)
+			# print (fstring)
+	print (n)
 
 def main():
 	# file1 = open('datasets/sample_sentences.txt', 'r')
