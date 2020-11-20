6d610de70c5f4bf961dbdba39d7db7b4369b6dae
Flavio Miceli <flavio.armando.miceli@gmail.com>
2020-11-20 12:07:23 +0100

    Rewrote Corpus code so it doesn't keep the entire corpus in memore, but instead provides a datastream using original file on harddrive to generate lines.

--- n_gram.py
+++ n_gram.py
@@ -1,85 +1,88 @@
 from collections import defaultdict
+import json
+import string
+from epitran import Epitran
 
 class Corpus:
 
-	def __init__(self, path, epi):
+	def __init__(self, path, epi_code):
+		self._file = path
+		self._dict_file = 'g2p_dictionary/dutch_dic_to_phonetic.1.json'
+		self._epi_code = epi_code
 		self._count_table = defaultdict(int)
-		self._corpus = list()
-		self._vocab = set()
 
-		self.set_corpus(path, epi)
-		self.update_count_table()
+		self.set_corpus()
 
-	def set_corpus(self,
-			path, epi_code, dict_path='g2p_dictionary/dutch_dic_to_phonetic.1.json'):
-		def clean_line(line):
-			import string
+	def clean_line(self, line):
+		# remove punctuation
+		line = line.translate(str.maketrans('', '', string.punctuation))
+		# to lower case
+		line = line.lower()
+		# remove nl character
+		line = line.replace("\n", "")
+		return line
 
-			# remove digits
-			line = line.translate(str.maketrans('', '', string.digits))
-			# remove punctuation
-			line = line.translate(str.maketrans('', '', string.punctuation))
-			# to lower case
-			line = line.lower()
-			# remove nl character
-			line = line.replace("\n", "")
-			return line
-
-		import json
-		from epitran import Epitran
-		with open(path, 'r') as text, \
-			open(dict_path, 'r') as g2p_file:
+	def set_corpus(self):
+		with open(self._file, 'r') as text, \
+			open(self._dict_file, 'r') as g2p_file:
 			g2p_dict = json.load(g2p_file)
 			g2p_dict = {k.lower():v for k,v in g2p_dict.items()}
-			epi = Epitran(epi_code)
+			epi = Epitran(self._epi_code)
 
 			for line in text:
-				line = clean_line(line)
+				line = self.clean_line(line)
 				# transliterate g2p using dict with epitran as fallback
-				if (use_dict):
-					line_phonetic = []
-					for word in line.split():
-						word_phonetic = g2p_dict.get(word, epi.transliterate(word))
-						line_phonetic.append(word_phonetic)
-					self._corpus.append(" ".join(line_phonetic))
-				# transliterate using epitran
-				else:
-					line_phonetic = epi.transliterate(line)
-					self._corpus.append(line_phonetic)
+				line_phonetic = []
+				for word in line.split():
+					word_phonetic = g2p_dict.get(word, epi.transliterate(word))
+					line_phonetic.append(word_phonetic)
+				for word in line_phonetic:
+					self._count_table[word] += 1
 
-	def update_count_table(self):
-		# Go through corpus line by line. Add occurences of each word to table.
-		self._count_table = defaultdict(int)
-		for line in self._corpus:
-			for word in line.split():
-				self._count_table[word] += 1
+	def update_count_table(self, line):
+		for word in line.split():
+			self._count_table[word] += 1
 
-		self._vocab = set(self._count_table.keys())
+	def get_data_stream(self, min_count=1, min_length=1):
+		with open(self._file, 'r') as text, \
+			open(self._dict_file, 'r') as g2p_file:
+			g2p_dict = json.load(g2p_file)
+			g2p_dict = {k.lower():v for k,v in g2p_dict.items()}
+			epi = Epitran(self._epi_code)
 
-	def prune_corpus(self, min_count=1, min_length=1):
-		if (min_count > 1):
-			self.rm_lines_with_rare_words(min_count)
-		if (min_length > 1):
-			self.rm_short_lines(min_length)
+			for line in text:
+				line = self.clean_line(line)
+				# transliterate g2p using dict with epitran as fallback
+				line_phonetic = []
+				for word in line.split():
+					word_phonetic = g2p_dict.get(word, epi.transliterate(word))
+					line_phonetic.append(word_phonetic)
+				if (self.validate_line(" ".join(line_phonetic), min_count, min_length)):
+					yield (line_phonetic)
 
-	def rm_lines_with_rare_words(self, min_count=1):
-		# Store each line in corpus where every word occurs at least min_count times
-		# in the original corpus.
-		self._corpus = [line for line in self._corpus if \
-			all([self._count_table[word] >= min_count for word in line.split()])]
-		self.update_count_table()
+	def	validate_line(self, line, min_count=1, min_length=1):
+		if (any([c.isdigit() for c in line])):
+			return False
+		if (len(line.split()) < min_length):
+			return False
+		if (any([self._count_table[word] < min_count for word in line.split()])):
+			return False
+		return True
 
-	def rm_short_lines(self, min_length=1):
-		self._corpus = \
-			[line for line in self._corpus if len(line.split()) >= min_length]
-		self.update_count_table()
+	def get_vocab(self, min_count=1, min_length=1):
+		vocab = set()
+		for line in self.get_data_stream(min_count, min_length):
+			vocab.update(line)
+		return vocab
 
-	def print_corpus(self):
-		for line in self._corpus:
-			print (line)
+	def print_vocab(self, min_count=1, min_length=1):
+		print ()
+		print(sorted(self.get_vocab(min_count, min_length)))
 
-	def print_vocab(self):
-		print(sorted(self._vocab))
+	def print_corpus(self, min_count=1, min_length=1):
+		print ()
+		for line in self.get_data_stream(min_count, min_length):
+			print (" ".join(line))
 
 class NGram_LM:
 
@@ -88,33 +91,35 @@ class NGram_LM:
 		self._count_table = dict()
 		self._prob_table = dict()
 
-	def preprocess_sentence(self, text):
-		for line in text:
-			yield [c for c in line]
-
-	def preprocess_history(self, history):
-		# Used to select the end of a longer sentence, or pad the start of a
-		# sentence with <s> tokens
-		if (len(history) == self._order):
-            return (tuple(history))
-		elif (len(history) > self._order):
-            return (tuple(history[len(history) - self._order:length]))
-		else:
-            missing = self._order - len(history)
-            return (tuple(['<s>'] * missing) + tuple(history))
-
-	def count_ngrams(self, text):
-		for line in text:
-			for i in range(len(line)):
-				phoneme = line[i]
-				ngram = self.preprocess_history(line[:i])
-				if ngram in self._count_table:
-					if phoneme in self._count_table[ngram]:
-                        self._count_table[ngram][phoneme] += 1
-                    else:
-                        self._count_table[ngram][phoneme] = 1
-				else:
-                    self._count_table[ngram] = {phoneme: 1}
+	# def preprocess_sentence(self, text):
+	# 	for line in text:
+	# 		yield (tuple("<BoS>" * self._order) +
+	# 				tuple([c for c in line]) +
+	# 				tuple("<EoS>" * self._order))
+	#
+	# def preprocess_history(self, history):
+	# 	# Used to select the end of a longer sentence, or pad the start of a
+	# 	# sentence with <s> tokens
+	# 	if (len(history) == self._order):
+    #         return (tuple(history))
+	# 	elif (len(history) > self._order):
+    #         return (tuple(history[len(history) - self._order:length]))
+	# 	else:
+    #         missing = self._order - len(history)
+    #         return (tuple(['<BoS>'] * missing) + tuple(history))
+	#
+	# def count_ngrams(self, text):
+	# 	for line in text:
+	# 		for i in range(len(line)):
+	# 			phoneme = line[i]
+	# 			ngram = self.preprocess_history(line[:i])
+	# 			if (ngram in self._count_table):
+	# 				if (phoneme in self._count_table[ngram]):
+    #                     self._count_table[ngram][phoneme] += 1
+    #                 else:
+    #                     self._count_table[ngram][phoneme] = 1
+	# 			else:
+    #                 self._count_table[ngram] = {phoneme: 1}
 
 	def set_prob_table(self):
 		self._prob_table = self._count_table
@@ -143,21 +148,22 @@ def main():
 
 	# Output corpus info if verbose
 	if (args.verbose):
-		print("\nBefore pruning:")
+		print("\nWithout pruning:")
 		print("Sentences: {}\nUnique words: {}" \
-				.format(len(corp._corpus), len(corp._vocab)))
-
-	# Prune sentences
-	corp.prune_corpus(min_count=args.count, min_length=args.length)
+				.format(len([line for line in corp.get_data_stream()]),
+						len(corp.get_vocab())))
 
 	# Output corpus info if verbose
 	if (args.verbose):
-		print("\nAfter pruning:")
+		print("\nWith pruning:")
 		print("Sentences: {}\nUnique words: {}" \
-				.format(len(corp._corpus), len(corp._vocab)))
+				.format(len([line for line in corp.get_data_stream(
+							min_count=args.count, min_length=args.length)]),
+						len(corp.get_vocab(
+							min_count=args.count, min_length=args.length))))
 
-	# corp.print_corpus()
-	# corp.print_vocab()
+	corp.print_corpus(min_count=args.count, min_length=args.length)
+	corp.print_vocab(min_count=args.count, min_length=args.length)
 
 if __name__ == "__main__":
 	main()
